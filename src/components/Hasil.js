8cb3d81728337b90bec87118a5cddff6d9020ea5
ahmadrpl <ahmad.rpl001@gmail.com>
2021-01-09 01:03:22 +0700

    membuat modal edit pesanan

--- src/components/Hasil.js
+++ src/components/Hasil.js
@@ -1,8 +1,61 @@
 import React, { Component } from "react";
 import { Badge, Col, ListGroup, Row } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
+import ModalKeranjang from "./ModalKeranjang";
+import TotalBayar from "./TotalBayar";
 
 export default class Hasil extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+      keranjangDetail: false,
+      jumlah: 0,
+      keterangan: "",
+    };
+  }
+
+  handleShow = (menuKeranjang) => {
+    this.setState({
+      showModal: true,
+      keranjangDetail: menuKeranjang,
+      jumlah: menuKeranjang.jumlah,
+      keterangan: menuKeranjang.keterangan,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  tambah = () => {
+    this.setState({
+      jumlah: this.state.jumlah + 1,
+    });
+  };
+
+  kurang = () => {
+    if (this.state.jumlah !== 1) {
+      this.setState({
+        jumlah: this.state.jumlah - 1,
+      });
+    }
+  };
+
+  changeHandler = (event) => {
+    this.setState({
+      keterangan: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("test", this.state.keterangan);
+  };
   render() {
     const { keranjangs } = this.props;
     return (
@@ -11,14 +64,19 @@ export default class Hasil extends Component {
           <strong>Hasil</strong>
         </h4>
         <hr />
-        {keranjangs.length !== 0 && 
+        {keranjangs.length !== 0 && (
           <ListGroup variant="flush">
             {keranjangs.map((menuKeranjang) => (
-              <ListGroup.Item>
+              <ListGroup.Item
+                key={menuKeranjang.id}
+                onClick={() => this.handleShow(menuKeranjang)}
+              >
                 <Row>
                   <Col xs="2">
                     <h4>
-                      <Badge pill variant="success">{menuKeranjang.jumlah}</Badge>
+                      <Badge pill variant="success">
+                        {menuKeranjang.jumlah}
+                      </Badge>
                     </h4>
                   </Col>
                   <Col>
@@ -26,13 +84,24 @@ export default class Hasil extends Component {
                     <p>Rp. {numberWithCommas(menuKeranjang.product.harga)}</p>
                   </Col>
                   <Col>
-                  <strong className="float-right">Rp. {numberWithCommas(menuKeranjang.total_harga)}</strong>
+                    <strong className="float-right">
+                      Rp. {numberWithCommas(menuKeranjang.total_harga)}
+                    </strong>
                   </Col>
                 </Row>
               </ListGroup.Item>
             ))}
+            <TotalBayar keranjangs={keranjangs} {...this.props} />
+            <ModalKeranjang
+              handleClose={this.handleClose}
+              {...this.state}
+              tambah={this.tambah}
+              kurang={this.kurang}
+              changeHandler={this.changeHandler}
+              handleSubmit={this.handleSubmit}
+            />
           </ListGroup>
-        }
+        )}
       </Col>
     );
   }
--- src/pages/Sukses.js
+++ src/pages/Sukses.js
@@ -1,22 +1,41 @@
-import React, { Component } from 'react'
-import { Button, Image } from 'react-bootstrap'
+import React, { Component } from "react";
+import { Button, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHeart
-} from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { API_URL } from "../utils/contants";
 
 export default class Sukses extends Component {
-    render() {
-        return (
-            <div className="mt-4 text-center">
-                <Image src="assets/images/sukses.png" width="500" />
-                <h2>Sukses Pesan</h2>
-                <p>Terimakasih sudah memesan <FontAwesomeIcon icon={faHeart} className="mr-2 text-danger" /></p>
-                <Button variant="primary" as={Link} to="/">
-                    Kembali
-                </Button>
-            </div>
-        )
-    }
+  componentDidMount() {
+    axios
+      .get(API_URL + "keranjangs")
+      .then((res) => {
+        const keranjangs = res.data;
+        keranjangs.map(function(item){
+            return axios
+            .delete(API_URL+"keranjangs/"+item.id)
+            .then((res) => console.log(res))
+            .catch((error) => console.log(error))
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div className="mt-4 text-center">
+        <Image src="assets/images/sukses.png" width="500" />
+        <h2>Sukses Pesan</h2>
+        <p>
+          Terimakasih sudah memesan{" "}
+          <FontAwesomeIcon icon={faHeart} className="mr-2 text-danger" />
+        </p>
+        <Button variant="primary" as={Link} to="/">
+          Kembali
+        </Button>
+      </div>
+    );
+  }
 }
